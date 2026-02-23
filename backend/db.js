9b93982d4fc0538371d7d4fa36ed68cd9991c266
Csukárdi Rajmund <csukardi.rajmund@ganziskola.hu>
2026-02-23 10:31:09 +0100

    a

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   connectionLimit: 10,
 });
